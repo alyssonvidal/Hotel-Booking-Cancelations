076ce7a8a53d80349931bbd60e2d4685e5cdb2a0
alyssonvidal <alyssonmach@gmail.com>
2023-06-10 08:18:30 -0300

    teste

--- src/train.py
+++ src/train.py
@@ -1,33 +1,15 @@
 # Basic Tools
 import pandas as pd
 import numpy as np
-from datetime import datetime as dt
-
-# Visualization Tools
-from matplotlib import pyplot as plt
-import seaborn as sns
 
 # File / OS tools
 import json
 import os
 
-# Machine Learning Models
-#from xgboost import XGBClassifier
+# Model
 from lightgbm import LGBMClassifier
 
-# Preprocessing Tools
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, PowerTransformer, OrdinalEncoder, OneHotEncoder, FunctionTransformer, LabelEncoder
-#from category_encoders.count import CountEncoder
-
-# Model Selection Tools
-from sklearn.model_selection import cross_val_score, cross_val_predict, KFold, cross_validate, StratifiedKFold
-
-
-# Model Evaluation Tools
-from sklearn.metrics import accuracy_score, recall_score, precision_score, roc_auc_score, f1_score
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, roc_auc_score, roc_curve, auc, classification_report
-
-
+# Project/Developer Libraries
 from config import Path
 from utils import Preparation, Plots, MachineLearning, Metrics, HyperTuning
 
@@ -39,7 +21,6 @@ np.random.seed(RANDOM_SEED)
 
 data = pd.read_csv(Path.DATA_PROCESSED_PATH)
 data_prep = data.copy()
-data_prep = data_prep.sample(15000)
 
 ## Data Preparation
 data_prep = Preparation.Encoding(data_prep)
@@ -48,8 +29,6 @@ SELECTED_FEATURES, DROP_FEATURES, TARGET = Preparation.SelectedFeatures(data_pre
 
 y = data_prep[TARGET]
 X = data_prep[SELECTED_FEATURES]
-
-
 
 # Hyperparamters Tuning
 hypeparamters = HyperTuning(X, y)
